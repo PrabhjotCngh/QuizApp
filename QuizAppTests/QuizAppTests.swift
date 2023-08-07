00aba8112b6eb460a3f8711828c5853a029c4021
Prabjot Singh <prabjot.singh@andersencorp.com>
2023-08-07 07:53:55 -0400

    Written test to grade exam based on scores

--- QuizAppTests/QuizAppTests.swift
+++ QuizAppTests/QuizAppTests.swift
@@ -11,7 +11,6 @@ import XCTest
 final class QuizAppTests: XCTestCase {
 
     func test_should_make_sure_quiz_total_points_are_calculated_correctly() {
-        
         let quizesDTOs = QuizData.loadQuizDTOs()
         let quizes = quizesDTOs.map(Quiz.init)
         
@@ -21,7 +20,22 @@ final class QuizAppTests: XCTestCase {
         
         XCTAssertEqual(3, mathQuiz.questions.count)
         XCTAssertEqual(30, mathQuiz.totalPoints)
-        
     }
+}
 
+class when_calculate_student_grade: XCTestCase {
+    
+    func test_calculate_grade_successfully_based_on_student_score() {
+        let quizesDTOs = QuizData.loadQuizDTOs()
+        let quizes = quizesDTOs.map(Quiz.init)
+        
+        let mathQuiz = quizes.first {
+            $0.quizId == 1
+        }!
+        
+        XCTAssertEqual("A", mathQuiz.calculateLetterGrade(score: 90))
+        XCTAssertEqual("B", mathQuiz.calculateLetterGrade(score: 72))
+        XCTAssertEqual("F", mathQuiz.calculateLetterGrade(score: 42))
+    }
+    
 }
