8a376f55de82272ccbd0ddc74eccf874b3d2b938
Prabjot Singh <prabjot.singh@andersencorp.com>
2023-08-07 06:36:44 -0400

    Project setup

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by M_2195552 on 2023-08-07.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
